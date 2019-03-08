d71c5bd4ed9929431c1e4218a064b727a9b30fb7
Zeff020 <zefwolffs@gmail.com>
2019-03-08 13:26:35 +0100

    Added option to add images to posts.

--- blog/models.py
+++ blog/models.py
@@ -10,6 +10,7 @@ class Post(models.Model):
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='post_pics', blank=True, null=True)
 
     def __str__(self):
         return self.title
